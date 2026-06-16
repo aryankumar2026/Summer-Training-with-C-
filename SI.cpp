8662f2ec08376bfd4a7c2f866981dd494837a790
aryankumar2026 <yeshumasih00002@gmail.com>
2026-06-16 21:43:26 +0530

    Update SI.cpp

--- SI.cpp
+++ SI.cpp
@@ -3,8 +3,9 @@ using namespace std;
 int main()
 {
     float p,r,t,si;
+    cout<<"enter three numbers:"; 
     cin>>p>>r>>t;
     si=(p*t*r);
-    cout<<si;
+    cout<<"si="<<si;
     return 0;
 }
