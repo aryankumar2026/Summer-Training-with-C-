007eca6b865b0bf1677f186544eff1a555f25f78
Aryan Kumar <yeshumasih00002@gmail.com>
2026-06-30 22:47:12 +0530

    Update C2F.cpp

--- C2F.cpp
+++ C2F.cpp
@@ -3,9 +3,9 @@ using namespace std;
 int main()
 {
     float c,f;
-    cout<<"enter the temperature:";
+    cout<<"enter the temperature:"<< endl;
     cin>>c;
     f=(c*9.0/5)+32;
-    cout<<"the temperature in f:"<<f;
+    cout<<"the temperature in f:"<<f<< endl;
     return 0;
 }
