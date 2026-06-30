4b5585b1e8d12229b054621ea52bcfff5cb9c0d7
Aryan Kumar <yeshumasih00002@gmail.com>
2026-06-30 22:50:28 +0530

    Update leapyear.cpp

--- leapyear.cpp
+++ leapyear.cpp
@@ -3,15 +3,15 @@ using namespace std;
 int main()
 {
     int year;
-    cout<<"enter year:";
+    cout<<"enter year:"<< endl;
     cin>>year;
     if(year%400==0)
-    cout<<"leap year";
+    cout<<"leap year"<< endl;
     else if(year%100==0)
-    cout<<"leap year";
+    cout<<"leap year"<< endl;
     else if(year%4==0)
-    cout<<"leap year";
+    cout<<"leap year"<< endl;
     else
-    cout<<"not a leap year";
+    cout<<"not a leap year"<< endl;
     return 0;
 }
