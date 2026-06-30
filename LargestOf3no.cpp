0c8312704b8a6986c5751a0461b404223da4ab94
Aryan Kumar <yeshumasih00002@gmail.com>
2026-06-30 22:48:32 +0530

    Update LargestOf3no.cpp

--- LargestOf3no.cpp
+++ LargestOf3no.cpp
@@ -3,21 +3,21 @@ using namespace std;
 int main()
 {
     int a,b,c;
-    cout<<"enter three numbers:";
+    cout<<"enter three numbers:"<< endl;
     cin>>a>>b>>c;
     if(a>b)
     {
         if(a>c)
-        cout<<"largest number:"<<a;
+        cout<<"largest number:"<<a<< endl;
         else
-        cout<<"largest number:"<<c;
+        cout<<"largest number:"<<c<< endl;
     }
     else
     { 
      if(b>c)
-     cout<<"largest number:"<<b;
+     cout<<"largest number:"<<b<< endl;
      else
-     cout<<"largest number:"<<c;
+     cout<<"largest number:"<<c<< endl;
     }
     return 0;
 }
