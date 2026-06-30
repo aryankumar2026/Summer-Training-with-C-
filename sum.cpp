b0638d8dfb5df23c8b35dde7922b7769636beadf
Aryan Kumar <yeshumasih00002@gmail.com>
2026-06-30 22:50:51 +0530

    Update sum.cpp

--- sum.cpp
+++ sum.cpp
@@ -3,8 +3,8 @@ using namespace std;
 int main()
 {
     int a,b;
-    cout<<"enter two numbers:";
+    cout<<"enter two numbers:"<< endl;
     cin>>a>>b;
-    cout<<"sum="<<a+b;
+    cout<<"sum="<<a+b<< endl;
 return 0;
 }
